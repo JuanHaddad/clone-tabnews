acd868f6a9c0d2abbcc02059b55f02176867c705
Juan Hernandes Haddad Pavani <101416678+JuanHaddad@users.noreply.github.com>
2023-07-11 23:11:56 +0000

    adicionando `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendendo a fazer um projeto do 0, desde o início até o deploy como site.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function Home(){
-    return <h1>Oi mor te amo muito, se vc me ama me manda uma foto sorrindo hehe</h1>
+function Home() {
+  return (
+    <h1>Oi mor te amo muito, se vc me ama me manda uma foto sorrindo hehe</h1>
+  );
 }
 
 export default Home;
